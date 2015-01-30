7777d8e1fcf9053485e4b5268abb499e3287a640
peterhad313 <peterhaddad2016@u.northwestern.edu>
2015-01-30 11:32:50 -0600

    add top level entity

---
+++ asgn2/display_divider.vhd
@@ -0,0 +1,15 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+--Additional standard or custom libraries go here
+entity display_divider is
+port(
+ --You will replace these with your actual inputs and outputs
+ inputs : in std_logic;
+ outputs : out std_logic
+ );
+end entity display_divider;
+architecture structural of display_divider is
+--Signals and components go here
+begin
+--Structural design goes here
+end architecture structural; 

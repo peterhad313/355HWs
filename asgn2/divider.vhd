ddeccd211aae07681a641e571e7cd280fe88d5ca
Hannah Arntson <hannaharntson@gmail.com>
2015-02-06 11:14:01 -0600

    Final divider file

--- asgn2/divider.vhd
+++ asgn2/divider.vhd
@@ -22,15 +22,12 @@ end entity divider;
 architecture structural_combinational of divider is
 	--Signals
 	type data_array is array(0 to (DIVIDEND_WIDTH-1)) of std_logic_vector(DIVISOR_WIDTH downto 0);
- --SEDA_COMMENT: I introduced an additional array for comparator outputs
  	type data_array_short is array(1 to DIVIDEND_WIDTH) of std_logic_vector(DIVISOR_WIDTH-1 downto 0);
 	signal datal_array : data_array;
-  signal comp_out_array: data_array_short;
+  	signal comp_out_array: data_array_short;
 	signal dout_temp : std_logic_vector(DIVISOR_WIDTH-1 downto 0);
---	signal dividend_temp : std_logic_vector (DIVIDEND_WIDTH - 1 downto 0);
---	signal divisor_temp : std_logic_vector (DIVISOR_WIDTH - 1 downto 0);
 	signal quo_temp : std_logic_vector (DIVIDEND_WIDTH - 1 downto 0);
-  signal rem_temp : std_logic_vector (DIVISOR_WIDTH - 1 downto 0);
+  	signal rem_temp : std_logic_vector (DIVISOR_WIDTH - 1 downto 0);
 
 	--Components
 	component comparator
@@ -46,35 +43,27 @@ architecture structural_combinational of divider is
 	end component comparator;
 
 begin
-	--process for start button
+	--process for start button and overflow
 	process(start,dividend,divisor)
 	begin
-		if start='1' then
-		  
-		  
---		  overflow <= divisor(0);
---			dividend_temp<=dividend;
---			divisor_temp<=divisor;
---SEDA_COMMENT: need to compute overflow with start button control as well
---SEDA_COMMENT: tried controlling release of output rather than input			
-		if (to_integer(unsigned(divisor))=0) then
-			overflow<='1';
-			quotient<= quo_temp;
-		  remainder <= rem_temp;
-		else 
-			overflow<='0';
-			quotient<= quo_temp;
-		  remainder <= rem_temp;
-		end if;
+		if start='1' then		
+			if (to_integer(unsigned(divisor))=0) then
+				overflow<='1';
+				quotient<= quo_temp;
+			  remainder <= rem_temp;
+			else 
+				overflow<='0';
+				quotient<= quo_temp;
+			  	remainder <= rem_temp;
+			end if;
 		end if;
 	end process;
 
 	--set first element of datal_array	
-	-- add divisor length 0's
-datal_array(0)<="0000"&dividend(DIVIDEND_WIDTH-1); --concatenate '0' at beginning to get correct vector length
+	datal_array(0)<="0000"&dividend(DIVIDEND_WIDTH-1); --concatenate '0' at beginning to get correct vector length
  
---
---	--network of comparators
+
+	--network of comparators
 	COMPARE:
 	for i in 1 to (DIVIDEND_WIDTH-1) generate
 		comp: comparator
@@ -83,33 +72,16 @@ datal_array(0)<="0000"&dividend(DIVIDEND_WIDTH-1); --concatenate '0' at beginnin
 			DINR=>divisor, 
 			DOUT=>comp_out_array(i), 
 			isGreaterEq=>quo_temp(DIVIDEND_WIDTH-i));
-----
-datal_array(i) <= comp_out_array(i)&dividend(DIVIDEND_WIDTH-1-i);
---SEDA_COMMENT: AT THE LAST ITERATION THIS WILL PLACE THE LAST DINL value to the array
+			datal_array(i) <= comp_out_array(i)&dividend(DIVIDEND_WIDTH-1-i);
 	end generate;
 --	
---	--SEDA_COMMENT: NEED TO ASSIGN THE LAST BIT OF DIVISION RESULT HERE, USING THE LAST VALUE OF THE ARRAY
+	--Assign last bit of division
 	comp: comparator
 			generic map (DATA_WIDTH=>DIVISOR_WIDTH)
 			port map (DINL=>datal_array(DIVIDEND_WIDTH-1),
 			DINR=>divisor, 
 			DOUT=>rem_temp, 
 			isGreaterEq=>quo_temp(0));
-
-	--set remainder
-----	remainder<=datal_array(DIVIDEND_WIDTH-DIVISOR_WIDTH)(DIVISOR_WIDTH-1 downto 0);
-----
-----process for overflow check
-----	process(start, dividend, divisor)
-----	begin
---		--set overflow
-----		if (to_integer(unsigned(divisor))=0) then
-----			overflow<='1';
-----		else 
-----			overflow<='0';
-----		end if;
-----	end process;
-
 
 end architecture structural_combinational;
 ----------------------------------------------------------------------------- 
